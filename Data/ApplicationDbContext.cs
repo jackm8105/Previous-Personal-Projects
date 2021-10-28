72385cb4a94bc3984f86e54e4d448131536c31b9
jackm8105 <91294308+jackm8105@users.noreply.github.com>
2021-10-27 18:58:21 -0600

    Add files via upload

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using tutorialJokesWebApp.Models;
+
+namespace tutorialJokesWebApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<tutorialJokesWebApp.Models.Joke> Joke { get; set; }
+    }
+}
